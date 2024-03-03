54da8295998e86c52e7649e2cfe36a9cdb96a4be
Aravinda Reddy <aravind4532@gmail.com>
2024-03-03 16:15:44 -0500

    This Map reduce does augmentation on output from previous two responses

---
+++ code/2DataFilteringAndAugmentation/src/main/java/com/example/AugmentJobDriver.java
@@ -0,0 +1,36 @@
+package com.example;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.example.mapper.AugmentMapper;
+import com.example.reducer.AugmentReducer;
+
+public class AugmentJobDriver {
+    public static void main(String[] args) throws Exception {
+    	if (args.length < 4) {
+            System.err.println("Usage: AugmentJobDriver <input path> <output path> <GeoGraphic Metadata file> ");
+            System.exit(-1);
+        }
+
+        Configuration conf = new Configuration();
+        conf.set("geographic.metadata.path", args[2]);
+        
+        
+        Job job = Job.getInstance(conf, "Augment GHCN Data");
+        job.setJarByClass(AugmentJobDriver.class);
+        job.setMapperClass(AugmentMapper.class);
+        job.setReducerClass(AugmentReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ code/2DataFilteringAndAugmentation/src/main/java/com/example/mapper/AugmentMapper.java
@@ -0,0 +1,83 @@
+package com.example.mapper;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AugmentMapper extends Mapper<LongWritable, Text, Text, Text> {
+    
+	private final Map<String, String> geoGraphicMetadata = new HashMap<>();
+
+    @Override
+    protected void setup(Context context) throws IOException {
+        Configuration conf = context.getConfiguration();
+        String stateFilePath = conf.get("geographic.metadata.path");
+
+        loadGeoGraphicMetadata(conf, stateFilePath);
+    }
+
+	private void loadGeoGraphicMetadata(Configuration conf, String hdfsFilePath) {
+		Path path = new Path(hdfsFilePath);
+		try(FileSystem fs = FileSystem.get(conf);){
+		
+        
+    	try (BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(path)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                
+            	int firstCommaIndex = line.indexOf(",");
+                if (firstCommaIndex == -1) {
+                   
+                    return;
+                }
+
+                String stationId = line.substring(0, firstCommaIndex).trim();
+                String remainingData = line.substring(firstCommaIndex + 1).trim();
+                
+                geoGraphicMetadata.put(stationId, remainingData);
+            	
+            	
+            }
+        }
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+	    String line = value.toString();
+	    // Split the input line at the first comma to separate the station ID from the rest
+	    int firstCommaIndex = line.indexOf(",");
+	    if (firstCommaIndex == -1) {
+	        // Log or handle the error if needed
+	        return;
+	    }
+
+	    String stationId = line.substring(0, firstCommaIndex).trim();
+	    String remainingData = line.substring(firstCommaIndex + 1).trim();
+	    String geographicInformation = geoGraphicMetadata.getOrDefault(stationId, "N/A,N/A,N/A"); // Assuming format: state,location,country
+
+	    // Concatenate the original data with the geographic information
+	    String augmentedData = remainingData + "," + geographicInformation;
+
+	    // Emit the augmented data
+	    context.write(new Text(stationId), new Text(augmentedData));
+	}
+
+    
+    
+    
+    
+    
+}
+
